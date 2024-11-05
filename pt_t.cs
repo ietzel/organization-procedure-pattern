1761daf41ee656257c88a76a20a44c92b654fad2
ietzel <67884277+ietzel@users.noreply.github.com>
2024-11-05 17:43:37 -0500

    Update and rename pt_t.cs to index.html

--- pt_t.cs
+++
@@ -1,5 +0,0 @@
-public class PT_T {
-    public static void Main(string[] args) {
-        System.Console.WriteLine("1wX XSTJ, 2wX XNFJ, 3wX XNTJ, 4wX XSFP, 5wX XSTP, 6wX XXXX, 7wX XNFP, 8wX XNTP, 9wX XSFJ.");
-    }
-}
